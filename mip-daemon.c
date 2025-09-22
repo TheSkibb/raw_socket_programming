f78b10cd4a2abe6155369a416e507f6eb7c81d46
theskibb <kristfsk@uio.no>
2025-09-22 13:53:53 +0200

    proper argument handling

--- mip-daemon.c
+++ mip-daemon.c
@@ -9,8 +9,6 @@
 #include "lib/mip.h"
 #include "lib/utils.h"
 
-//TODO: remove send/recv mode, change for handle
-
 int printhelp(){
     printf("usage mipd [-h] [-d] <socket_upper> <MIP address>\n");
     return 0;
@@ -28,55 +26,18 @@ int create_epoll_table(){
     return epollfd;
 }
 
-#define BUFFER_SIZE 256 // Define a buffer size for incoming messages
-
-void handle_unix_socket_message(int unix_sockfd) {
-    char buffer[BUFFER_SIZE];
-    memset(buffer, 0, BUFFER_SIZE); // Clear the buffer
-    int data_socket;
-
-    // Accept a new connection
-    data_socket = accept(unix_sockfd, NULL, NULL);
-    if (data_socket == -1) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-    // Wait for next data packet from the accepted socket
-    int rc = read(data_socket, buffer, sizeof(buffer) - 1); // Leave space for null-termination
-    if (rc == -1) {
-        perror("read");
-        close(data_socket); // Close the accepted socket on error
-        exit(EXIT_FAILURE);
-    }
-
-    // Properly handle the bytes read; ensure there's at least one byte read
-    if (rc > 0) {
-        // Null-terminate the string
-        buffer[rc] = '\0';
-        // Print the received message
-        printf("Received message on Unix socket: %s\n", buffer);
-    }
-
-    // Close the connected socket after done using it
-    close(data_socket);
-}
-
 int main(int argc, char *argv[]){
-    //TODO: do proper flag checking
-
     /* determine mode */
     if(argc <= 2){
         printf("arguments supplied, see usage with -h\n");
         return 1;
     }
 
-    int send = 0;
     char *arg;
-    char unixSocketName[106] = "";
+    char unixSocketName[MAX_UNIX_PATH_LENGTH] = "";
+    uint8_t mip_address = 0xFF;
 
     //parse cmdline arguments
-    //TODO: add mip address (i think, maybe in client and server instead)
     for(int i = 1; i < argc; i++){
         arg = argv[i];
         if(strcmp(arg, "-h") == 0){ //print help
@@ -85,17 +46,26 @@ int main(int argc, char *argv[]){
         }else if(strcmp(arg, "-d") == 0){ //enable debug prints
             set_debug(1);
             debugprint("you have started the daemon with debug prints");
-        }else if(strcmp(arg, "s") == 0){ //send mode TODO: remove, everything should be one mode
-            debugprint("send mode");
-            send = 1;
-        }else if(strcmp(arg, "r") == 0){ //receiver mode TODO
-            debugprint("recvmode");
-            send = 0;
         }else{ // name of unix socket
-            debugprint("starting a unix socket with %s", arg);
-            strncpy(unixSocketName, arg, strlen(arg)+1);
-            //return 1;
+            if(strcmp(unixSocketName, "") == 0){
+                debugprint("unix socket name: %s", arg);
+                strncpy(unixSocketName, arg, strlen(arg)+1);
+            }else if (strcmp(arg, "") != 0){
+                mip_address = atoi(arg); 
+                debugprint("argument %s, gives MIP address is: %d", arg, mip_address);
+            }
         }
+    }
+
+    //validate cmdline arguments
+    if(mip_address == 0xFF){ //255 is reserved for special case
+        printf("address 255 is reserved for broadcast\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(strcmp(unixSocketName, "") == 0){
+        printf("you need to specify a unix socket name");
+        exit(EXIT_FAILURE);
     }
 
     /* raw socket setup */
