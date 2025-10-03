fa7c626d3e15f3effecf6bdf36185d57f0d0dedb
theskibb <kristfsk@uio.no>
2025-10-03 11:55:14 +0200

    start of routing daemon setup

--- home-exam1/routing_daemon.c
+++ home-exam1/routing_daemon.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "lib/utils.h"
+#include "lib/sockets.h"
 
-int main(){
+void print_usage(){
+    printf("./routing_daemon <socket_lower>\n");
+}
+
+int main(int argc, char *argv[]){
     set_debug(1);
+    debugprint("=checking cmd arguments=====================");
+    if(argc <= 1){
+        printf("too few arguments");
+        print_usage();
+        return 1;
+    }
+
+    char socket_unix_name[MAX_UNIX_PATH_LENGTH] ="";
+    strncpy(socket_unix_name, argv[1], strlen(argv[1]) +1);
+
+    debugprint("unix socket name: %s", socket_unix_name);
+
+    debugprint("=======================================done=\n");
+
+    debugprint("=unix socket setup==========================");
+
+    int socket_unix = create_unix_socket(socket_unix_name, UNIX_SOCKET_MODE_CLIENT);
+
+    debugprint("=======================================done=\n");
+
 }
