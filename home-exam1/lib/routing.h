59e4da8a90c4f016eb4e1c86ae37c4f64b13dc71
theskibb <kristfsk@uio.no>
2025-10-13 21:20:06 +0200

    change out message types for defined arrays which can be used in unix_socket_sdu

--- home-exam1/lib/routing.h
+++ home-exam1/lib/routing.h
@@ -4,8 +4,20 @@
 #include <stdint.h>
 #include <sys/time.h>
 
+/*
+ * below are some messages which can be used as payload in the
+ * unix_sock_sdu from the sockets library.
+ * NOTICE: some of the messages will need you to 
+ */
+
 // A HELLO message for discovering neighbouring nodes.
+#define ROUTING_HELLO_MSG {0x48, 0x45, 0x4c} //HEL
+
 // An UPDATE message to advertise the routes in your routing table.
+// NOTICE: you need to add the routing table after this message
+#define ROUTING_UPDATE_MSG { 0x55, 0x50, 0x44} //UPD
+
+
 
 //   A REQUEST message for intra-host route lookups.
 //   <MIP address of the host itself (8 bits)> 
@@ -14,14 +26,7 @@
 //   <E (0x45, 8 bits)> 
 //   <Q (0x51, 8 bits)> 
 //   <MIP address to look up (8 bits)>
-struct routing_request_msg {
-    uint8_t     host_mip_addr;
-    uint8_t     TTL; //Time To Live
-    uint8_t     R; //0x52
-    uint8_t     E;
-    uint8_t     Q;
-    uint8_t     lookup_mip_addr;
-} __attribute__((packed));
+#define ROUTING_REQUEST_MSG {0x052, 0x45, 0x51, 0xFF} //REQ<mip address>
 
 //   A RESPONSE message for intra-host route responses. This message is fully specified in the next section.
 //   <MIP address of the host itself (8 bits)> 
@@ -30,25 +35,7 @@ struct routing_request_msg {
 //   <S (0x53, 8 bits)> 
 //   <P (0x50, 8 bits)> 
 //   <next hop MIP address (8 bits)>
-struct routing_response_msg{
-    uint8_t     host_mip_addr;
-    uint8_t     TTL; //Time To Live
-    uint8_t     R; //0x52
-    uint8_t     S; //0x53
-    uint8_t     P; //0x50
-    uint8_t     next_hop_mip_addr;
-} __attribute__((packed));
-
-struct routing_hello_msg{
-    uint8_t     host_mip_addr;
-    uint8_t     TTL;
-    uint8_t     H;
-    uint8_t     E;
-    uint8_t     L;
-} __attribute__((packed));
-
-struct forward_table {
-};
+#define ROUTING_RESPONSE_MSG {0x52, 0x53, 0x50, 0xFF} //RSP<mip address>
 
 struct routing_update_msg{
     uint8_t                 host_mip_addr;
